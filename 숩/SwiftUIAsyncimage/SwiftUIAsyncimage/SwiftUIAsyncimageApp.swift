d87bb2830408d76e9654092536d32216e124eb87
realhsb <realhsb@gmail.com>
2023-07-22 18:35:42 +0900

    ✨ [Asyncimage] 네트워크 통신으로 이미지 설정

---
+++ 숩/SwiftUIAsyncimage/SwiftUIAsyncimage/SwiftUIAsyncimageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAsyncimageApp.swift
+//  SwiftUIAsyncimage
+//
+//  Created by Subeen on 2023/07/13.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAsyncimageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
