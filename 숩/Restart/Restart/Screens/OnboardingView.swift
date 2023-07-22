d443795d5be80be92fbe0c2c5af0c09e9adc7f19
realhsb <realhsb@gmail.com>
2023-07-22 18:49:56 +0900

    ✨ Onboarding View 추가

---
+++ 숩/Restart/Restart/Screens/OnboardingView.swift
@@ -0,0 +1,192 @@
+//
+//  OnboardingView.swift
+//  Restart
+//
+//  Created by Subeen on 2023/07/18.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    // MARK: - PROPERTY
+    
+    @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
+    
+    @State private var buttonWidth: Double = UIScreen.main.bounds.width - 80
+    @State private var buttonOffset: CGFloat = 0 // 사용자가 드래그할 동안 상태 감지
+    @State private var isAnimating: Bool = false // 애니메이션 감지
+    @State private var imageOffset: CGSize = CGSize(width: 0, height: 0) // .zero
+    @State private var indicatorOpacity: Double = 1.0
+    @State private var textTitle: String = "Share."
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        ZStack {
+            Color("ColorBlue")
+                .ignoresSafeArea(.all, edges: .all)
+            VStack(spacing: 20) {
+                // MARK: - HEADER
+                
+                Spacer()
+                
+                VStack(spacing: 0) {
+                    Text(textTitle) // Text Value Change : SwiftUI considers this View the same regardless of value change.
+                        .font(.system(size: 60))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                        .transition(.opacity)
+                        .id(textTitle)  // use the ID method => tell SwiftUI that a view is NO LONGER the same view.
+                    
+                    Text("""
+                    It's not how much we give but how much love we put into giving.
+                    """)
+                    .font(.title2)
+                        .fontWeight(.light)
+                        .foregroundColor(.white)
+                        .multilineTextAlignment(.center)
+                        .padding(.horizontal, 10)
+                } //: HEADER
+                .opacity(isAnimating ? 1 : 0)
+                .offset(y: isAnimating ? 0 : -40)
+                .animation(.easeOut(duration: 1), value: isAnimating) // 애니메이션 마지막 1초동안 천천히 이동
+                
+                // MARK: - CENTER
+                
+                ZStack {
+                    CircleGroupView(ShapeColor: .white, ShapeOpacity: 0.2)
+                        .offset(x: imageOffset.width * -1)  // 사진의 반대 방향
+                        .blur(radius: abs(imageOffset.width / 5))   // 양수만 가능
+                        .animation(.easeOut(duration: 1), value: imageOffset)
+                    Image("character-1")
+                        .resizable()
+                        .scaledToFit()
+                        .opacity(isAnimating ? 1 : 0)
+                        .animation(.easeOut(duration: 0.5), value: isAnimating)
+                        .offset(x: imageOffset.width * 1.2, y: 0) // 곱하면 애니메이션 속도 가속
+                        .rotationEffect(.degrees(Double(imageOffset.width / 20)))
+                        .gesture(
+                            DragGesture()
+                                .onChanged { gesture in
+                                    if abs(imageOffset.width) <= 150 {
+                                        imageOffset = gesture.translation
+                                        
+                                        withAnimation(.linear(duration: 0.25)) {
+                                            indicatorOpacity = 0
+                                            textTitle = "Give."
+                                        }
+                                    }
+                                }
+                                .onEnded { _ in
+                                    imageOffset = .zero
+                                    
+                                    withAnimation(.linear(duration: 0.25)) {
+                                        indicatorOpacity = 1
+                                        textTitle = "Share."
+                                    }
+                                }
+                        ) //: GESTURE
+                        .animation(.easeOut(duration: 1), value: imageOffset)
+                } //: CENTER
+                .overlay(   // Overlay Parameter
+                    Image(systemName: "arrow.left.and.right.circle")
+                        .font(.system(size: 44, weight: .ultraLight))
+                        .foregroundColor(.white)
+                        .offset(y: 20)
+                        .opacity(isAnimating ? 1 : 0)
+                        .animation(.easeOut(duration: 1).delay(1), value: isAnimating)
+                        .opacity(indicatorOpacity)
+                    , alignment: .bottom    // Alignment Parameter
+                )
+                
+                Spacer()
+                
+                // MARK: - FOOTER
+                
+                ZStack {
+                    // PARTS OF THE CUSTOM BUTTON
+                    
+                    // 1. BACKGROUND (STATIC)
+                    
+                    Capsule()
+                        .fill(Color.white.opacity(0.2))
+                    
+                    Capsule()
+                        .fill(Color.white.opacity(0.2))
+                        .padding(8)
+                    
+                    // 2. CALL-TO-ACTION (STATIC)
+                    
+                    Text("Get Started")
+                        .font(.system(.title3, design: .rounded))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .offset(x: 20)
+                    
+                    // 3. CAPSULE (DYNAMIC WIDTH)
+                    
+                    HStack {
+                        Capsule()
+                            .fill(Color("ColorRed"))
+                            .frame(width: buttonOffset + 80) // offset의 기본값은 0이므로 80 더함.
+                        
+                        Spacer()
+                    }
+                    // 4. CIRCLE (DRAGGABLE)
+                    
+                    HStack {
+                        ZStack {
+                            Circle()
+                                .fill(Color("ColorRed"))
+                            Circle()
+                                .fill(.black.opacity(0.15))
+                                .padding(8)
+                            Image(systemName: "chevron.right.2")
+                                .font(.system(size: 24, weight: .bold))
+                        }
+                        .foregroundColor(.white)
+                        .frame(width: 80, height: 80, alignment: .center)
+                        .offset(x: buttonOffset)
+                        .gesture(
+                            DragGesture()
+                                .onChanged { gesture in
+                                    if gesture.translation.width > 0 && buttonOffset <= buttonWidth - 80 {
+                                        // 버튼이 좌에서 우로만 움직이게 함, 버튼이 80 x 80 이므로 80을 빼줌.
+                                        buttonOffset = gesture.translation.width
+                                    }
+                                }
+                            
+                                .onEnded { _ in
+                                    withAnimation(Animation.easeOut(duration: 0.4)) {
+                                        if buttonOffset > buttonWidth / 2 {
+                                            // 버튼이 구역의 오른쪽 절반에 넘어와 있다면, 버튼이 오른쪽 끝까지 이동
+                                            buttonOffset = buttonWidth - 80
+                                            isOnboardingViewActive = false
+                                        } else {
+                                            buttonOffset = 0
+                                        }
+                                    }
+                                }
+                        ) //: GESTURE
+                        
+                        Spacer()
+                    } //: HEADER
+                } //: FOOTER
+                .frame(width: buttonWidth, height: 80, alignment: .center)
+                .padding()
+                .opacity(isAnimating ? 1 : 0)
+                .offset(y: isAnimating ? 0 : 40)
+                .animation(.easeOut(duration: 1), value: isAnimating)
+            } //: VSTACK
+        } // : ZSTACK
+        .onAppear(perform: {
+            isAnimating = true
+        })
+    }
+}
+
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView()
+    }
+}
