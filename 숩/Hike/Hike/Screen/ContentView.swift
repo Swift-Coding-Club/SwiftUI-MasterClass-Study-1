381dfde6d92129a352fd7c64f1d0d711bebe68c2
realhsb <realhsb@gmail.com>
2023-07-28 14:27:52 +0900

    ✨ 기본 Content View

---
+++ 숩/Hike/Hike/Screen/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Hike
+//
+//  Created by Subeen on 2023/07/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        CardView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
