d304dcf7f2dadf9d8f18f65e922a56808bb9f43d
realhsb <realhsb@gmail.com>
2023-07-28 14:26:05 +0900

    🎉 Hike 프로젝트 생성

---
+++ 숩/Hike/Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Subeen on 2023/07/13.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
